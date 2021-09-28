3f88fe85f7c1973fea641153077123e9231723e7
Xrsirx5 <91307602+Xrsirx5@users.noreply.github.com>
2021-09-28 12:40:38 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -43,7 +43,7 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@A6rzuxpt69drdr6t9d5
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
